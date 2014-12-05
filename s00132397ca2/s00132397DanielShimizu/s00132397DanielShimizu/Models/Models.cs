96a58d4d385203efb4c2fb38513702eff4544091
Dankenshim <s00132397@mail.itsligo.ie>
2014-12-05 19:59:01 +0000

    DayTwo

--- s00132397ca2/s00132397DanielShimizu/s00132397DanielShimizu/Models/Models.cs
+++ s00132397ca2/s00132397DanielShimizu/s00132397DanielShimizu/Models/Models.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
@@ -8,32 +9,74 @@ using System.Web;
 namespace s00132397DanielShimizu.Models
 {
 
-     class CampDbInitialiser : DropCreateDatabaseAlways<FilmDb>
+    class CampDbInitialiser : DropCreateDatabaseAlways<FilmDb>
     {
         protected override void Seed(FilmDb context)
         {
-
+            var films = new List<Film>()
+            {
+                new Film() {Title = "22 Jump Street", ReleaseDate = DateTime.Parse("04/06/2014"),
+                Genre = Genre.Comedy, Actors = new List<Actor>()
+                {
+                    new Actor()
+                    { Name = "Channing Tatum",
+                    Sex = Sex.Male
+                    },
+                    new Actor()
+                    { Name = "Jonah Hill",
+                        Sex = Sex.Male                    
+                    }
+                }},
+                new Film(){Title = "Guardians of the Galaxy", ReleaseDate = DateTime.Parse("05/06/2014"),
+                Genre = Genre.Action, Actors = new List<Actor>()
+                {
+                    new Actor()
+                    { Name = "Chris Pratt",
+                    Sex = Sex.Male
+                    },
+                    new Actor()
+                    { Name = "Bradley Cooper",
+                    Sex = Sex.Male
+                    },
+                    new Actor()
+                    { Name = "Vin Diesel",
+                    Sex = Sex.Male
+                    },
+                    new Actor()
+                    { Name = "Zoe Saldana",
+                    Sex = Sex.Female
+                    },
+                    new Actor()
+                    { Name = "Dave Batista",
+                    Sex = Sex.Male
+                    }
+                }}
+            };
+            films.ForEach(flm => context.Films.Add(flm));
+            context.SaveChanges();
         }
     }
-    class FilmDb:DbContext
+    class FilmDb : DbContext
     {
         public DbSet<Film> Films { get; set; }
         public DbSet<Actor> Actors { get; set; }
-        
 
         public FilmDb()
-            :base("FilmDb")
-        {
-        }
+            : base("FilmDb")
+        { }
+    }
         public class Film
         {
             [Key]
-            public int FimlId { get; set; }
-            [Display(Name = "Camp Name"), Required]
+            public int FilmId { get; set; }
+            [Display(Name = "Film Name"), Required]
             public string Title { get; set; }
-            public virtual List<Actor> Actors { get; set; }
+            [DisplayName("Released"), DataType(DataType.Date),
+                    DisplayFormat(DataFormatString = "{0:dd/MM/yyy}")]
             public DateTime ReleaseDate { get; set; }
             public Genre Genre { get; set; }
+            public virtual List<Actor> Actors { get; set; }
+
         }
         public class Actor
         {
@@ -54,4 +97,3 @@ namespace s00132397DanielShimizu.Models
         }
 
     }
-}
